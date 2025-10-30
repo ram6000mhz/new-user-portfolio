527157705ea772daa38f3b5a2516b2f06e16ac0b
ram6000mhz <qesjyap@tip.edu.ph>
2025-10-30 19:50:48 +0800

    add css

--- user-portfolio/src/pages/Machineviewport.jsx
+++ user-portfolio/src/pages/Machineviewport.jsx
@@ -1,8 +1,13 @@
 
 export const Machineviewport=()=>{
     return(
-        <div className="w-full h-full">
-            Machine viewport
+        <div className="w-full h-full bg-background">
+            <h1 className="text-accent-text">
+                Machine viewport
+            </h1>
+            <h2 className="text-muted-text">
+                Machine viewport
+            </h2>
         </div>
     )
 }
