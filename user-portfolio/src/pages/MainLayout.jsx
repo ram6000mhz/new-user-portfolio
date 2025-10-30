8c6eb3d4aa5b1278eec9c1f111222766ed87d7f9
ram6000mhz <qesjyap@tip.edu.ph>
2025-10-30 19:51:06 +0800

    add css

--- user-portfolio/src/pages/MainLayout.jsx
+++ user-portfolio/src/pages/MainLayout.jsx
@@ -2,13 +2,13 @@ export const MainLayout = ({children}) => {
   
   return (
     <div className="w-full h-full flex flex-col">
-        <nav className="w-full h-[30px] bg-amber-400">
+        <nav className="w-full h-[50px] bg-foreground">
             navbar
         </nav>
         <main className="flex-1 overflow-auto">
             {children}
         </main>
-        <footer className="w-full h-[30px] bg-amber-400">
+        <footer className="w-full h-[50px] bg-amber-400">
             footer
         </footer>
     </div>
