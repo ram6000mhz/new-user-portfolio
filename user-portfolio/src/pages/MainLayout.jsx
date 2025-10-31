89cc53d3732956aca209add97211d863d899de79
ram6000mhz <qesjyap@tip.edu.ph>
2025-10-31 18:56:35 +0800

    update index.css

--- user-portfolio/src/pages/MainLayout.jsx
+++ user-portfolio/src/pages/MainLayout.jsx
@@ -2,14 +2,14 @@ export const MainLayout = ({children}) => {
   
   return (
     <div className="w-full h-full flex flex-col">
-        <nav className="w-full h-[50px] bg-foreground">
-            navbar
+        <nav className="w-full h-[60px] border-b border-muted-border flex flex-row items-center px-10">
+          navbar
         </nav>
         <main className="flex-1 overflow-auto">
             {children}
         </main>
-        <footer className="w-full h-[50px] bg-amber-400">
-            footer
+        <footer className="w-full h-[50px]">
+          footer
         </footer>
     </div>
   );
