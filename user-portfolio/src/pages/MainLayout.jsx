596ba599a26e81b9e683cb0e1d241230827ea4cc
ram6000mhz <qesjyap@tip.edu.ph>
2025-10-31 19:47:37 +0800

    responsive text and padding

--- user-portfolio/src/pages/MainLayout.jsx
+++ user-portfolio/src/pages/MainLayout.jsx
@@ -2,8 +2,8 @@ export const MainLayout = ({children}) => {
   
   return (
     <div className="w-full h-full flex flex-col">
-        <nav className="w-full h-[60px] border-b border-muted-border flex flex-row items-center px-10">
-          navbar
+        <nav className="w-full h-[60px] border-b border-muted-border flex flex-row items-center px-2 sm:px-4 md:px-6 lg:px-10">
+          <div class="text-xs sm:text-sm md:text-base lg:text-lg">Responsive text</div>
         </nav>
         <main className="flex-1 overflow-auto">
             {children}
