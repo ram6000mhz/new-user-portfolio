9c8984cff58275b5565c4ce0123542835cf12b48
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-01 18:53:01 +0800

    responsive boxes

--- user-portfolio/src/pages/Machineviewport.jsx
+++ user-portfolio/src/pages/Machineviewport.jsx
@@ -8,6 +8,16 @@ export const Machineviewport=()=>{
             <h2 className="text-muted-text">
                 Machine viewport
             </h2>
+            <div className="flex flex-row sm:flex-col flex-wrap gap-4 p-4">
+                <div className="bg-amber-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div1</h1></div>
+                <div className="bg-blue-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div2</h1></div>
+                <div className="bg-red-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div3</h1></div>
+            </div>
+            <div className="flex flex-row sm:flex-col flex-wrap gap-4 p-4">
+                <div className="bg-amber-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div1</h1></div>
+                <div className="bg-blue-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div2</h1></div>
+                <div className="bg-red-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div3</h1></div>
+            </div>
         </div>
     )
 }
