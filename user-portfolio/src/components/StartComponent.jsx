7f0b87d0d7e21001e63f84b63ca7d967983d0bbb
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-03 22:37:33 +0800

    add startcomponent

---
+++ user-portfolio/src/components/StartComponent.jsx
@@ -0,0 +1,9 @@
+export const StartComponent=()=>{
+    return(
+        <div className="flex flex-col w-full h-full">
+            <div className="w-full h-[30px] bg-amber-200">
+                
+            </div>
+        </div>
+    );
+};
