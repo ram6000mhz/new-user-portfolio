0f682af3807a8596019cdfef2a59341524f1e932
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-05 05:34:45 +0800

    add apps

--- user-portfolio/src/components/StartComponent.jsx
+++ user-portfolio/src/components/StartComponent.jsx
@@ -1,9 +1,40 @@
+import {Power,User,Phone} from "lucide-react";
+
 export const StartComponent=()=>{
     return(
         <div className="flex flex-col w-full h-full">
-            <div className="grow"></div>
-            <div className="w-full h-[30px] bg-foreground">
-                
+            <div className="grow grid grid-cols-3 gap-1 p-3 items-start">
+                <div className="flex flex-col justify-center items-center">
+                    <div className="w-[35px] h-[35px] bg-foreground flex justify-center items-center p-2 border border-muted-border rounded-md hover:bg-foreground-highlight cursor-pointer">
+                        <User className="!w-[35px] !h-[35px] text-accent-text"/>
+                    </div>
+                    <p className="text-xs mt-1 text-center text-accent-text">About Me</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <div className="w-[35px] h-[35px] bg-foreground flex justify-center items-center p-2 border border-muted-border rounded-md hover:bg-foreground-highlight cursor-pointer">
+                        <Phone className="!w-[35px] !h-[35px] text-accent-text"/>
+                    </div>
+                    <p className="text-xs mt-1 text-center text-accent-text">Contacts</p>
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    <div className="w-[35px] h-[35px] bg-foreground flex justify-center items-center p-2 border border-muted-border rounded-md hover:bg-foreground-highlight cursor-pointer">
+                        <Phone className="!w-[35px] !h-[35px] text-accent-text"/>
+                    </div>
+                    <p className="text-xs mt-1 text-center text-accent-text">Contacts</p>
+                </div>
+
+            </div>
+            <div className="w-full h-[45px] bg-foreground flex flex-row items-center border-t border-muted-border p-4 gap-2">
+                <div className="w-[30px] h-[30px] rounded-2xl bg-background">
+                    {/* User Avatar Placeholder */}
+                </div>
+                <p className="text-accent-text">
+                    Ethan
+                </p>
+                <div className="grow"></div>
+                <div className="w-[25px] h-[25px] p-1 hover:bg-foreground-highlight flex items-center justify-center rounded cursor-pointer">
+                    <Power className="!w-[20px] !h-[20px] text-accent-text"/>
+                </div>
             </div>
         </div>
     );
