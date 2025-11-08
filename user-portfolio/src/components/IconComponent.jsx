4c027f9fc298b61980353f3a12afe435ec435908
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-09 06:25:16 +0800

    icon refactor

--- user-portfolio/src/components/IconComponent.jsx
+++
@@ -1,8 +0,0 @@
-export const IconComponent = ({Children, Title}) => {
-    return (
-        <div className="flex flex-col items-center justify-center h-[60px] w-[75px] p-1 rounded-md hover:bg-foreground-highlight">
-            {Children}
-            <p className="text-center text-xs text-accent-text">{Title}</p>
-        </div>
-    )
-};
