957e474bc580bc43a45821263f260ddd31b4a75f
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-09 06:24:24 +0800

    icon refactor

--- user-portfolio/src/pages/Machineviewport.jsx
+++ user-portfolio/src/pages/Machineviewport.jsx
@@ -1,13 +1,40 @@
 import { Rnd } from "react-rnd";
-import { IconComponent } from "../components/IconComponent";
-import {Folder} from "lucide-react";
+import { IconComponent } from "../apps/IconComponent";
+import { apps } from "../apps/Applist";
 
 export const Machineviewport=()=>{
     return(
-        <div className="w-full h-full bg-background flex">
-            <Rnd bounds="parent">
-                <IconComponent Children={<Folder className="!h-[25px] !w-[25px] accent-accent-icon"/>} Title="My Projects"/>
-            </Rnd>
+        <div className="w-full h-full bg-background flex flex-col relative">
+            
+                {apps.map((app, index) => (
+                    <Rnd 
+                        onDragStart={(e, data) => {
+                            // Called when drag starts
+                            console.log('Drag started at:', data.x, data.y);
+                        }}
+                        
+                        onDrag={(e, data) => {
+                            // Called continuously while dragging
+                            console.log('Dragging at:', data.x, data.y);
+                        }}
+                        
+                        onDragStop={(e, data) => {
+                            // Called when drag ends
+                            console.log('Drag stopped at:', data.x, data.y);
+                        }}
+                        
+                        key={index}
+                        bounds="parent"
+                        enableResizing={false}
+                        default={{
+                            x: 0,
+                            y: index * 50,
+                        }}
+                    >
+                        <IconComponent Children={app.icon} Title={app.title}/>
+                    </Rnd>
+                ))}
+            
         </div>
     )
 }
