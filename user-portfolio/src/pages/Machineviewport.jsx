45535cee981b3afcf0487d4e8bc3c70c8a52c236
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-08 08:00:05 +0800

    create app icon

--- user-portfolio/src/pages/Machineviewport.jsx
+++ user-portfolio/src/pages/Machineviewport.jsx
@@ -1,23 +1,13 @@
+import { Rnd } from "react-rnd";
+import { IconComponent } from "../components/IconComponent";
+import {Folder} from "lucide-react";
 
 export const Machineviewport=()=>{
     return(
-        <div className="w-full h-full bg-background">
-            <h1 className="text-accent-text">
-                Machine viewport
-            </h1>
-            <h2 className="text-muted-text">
-                Machine viewport
-            </h2>
-            <div className="flex flex-col sm:flex-row gap-4 p-4">
-                <div className="bg-amber-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div1</h1></div>
-                <div className="bg-blue-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div2</h1></div>
-                <div className="bg-red-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div3</h1></div>
-            </div>
-            <div className="flex flex-col sm:flex-row gap-4 p-4">
-                <div className="bg-amber-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div1</h1></div>
-                <div className="bg-blue-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div2</h1></div>
-                <div className="bg-red-400 flex-1 min-w-[100px] min-h-[100px] max-h-[500px]"><h1>div3</h1></div>
-            </div>
+        <div className="w-full h-full bg-background flex">
+            <Rnd bounds="parent">
+                <IconComponent Children={<Folder className="!h-[25px] !w-[25px] accent-accent-icon"/>} Title="My Projects"/>
+            </Rnd>
         </div>
     )
 }
