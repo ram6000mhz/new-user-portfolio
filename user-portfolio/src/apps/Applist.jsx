ab39a2bdbee3cc25caacd887749c4c19af1b58ce
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-09 06:25:06 +0800

    icon refactor

---
+++ user-portfolio/src/apps/Applist.jsx
@@ -0,0 +1,12 @@
+import { Folder, User } from "lucide-react";
+
+export const apps = [
+    { 
+        icon: <User className="!h-[25px] !w-[25px] accent-accent-icon"/>, 
+        title: "About Me" 
+    },
+    { 
+        icon: <Folder className="!h-[25px] !w-[25px] accent-accent-icon"/>, 
+        title: "My Projects" 
+    },
+];
