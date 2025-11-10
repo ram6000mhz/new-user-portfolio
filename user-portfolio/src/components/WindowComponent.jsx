a31d86761f5c41d1253387f4e123d85fc9e48881
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-10 14:13:57 +0800

    update window functionality

--- user-portfolio/src/components/WindowComponent.jsx
+++ user-portfolio/src/components/WindowComponent.jsx
@@ -1,19 +1,33 @@
 import {X, Square, Minimize2} from "lucide-react";
-export const WindowComponent=({title,children})=>{
+export const WindowComponent=({title, children, onClose, onFullscreen, onWindow, onMinimize})=>{
+    const Terminate=()=>{
+        onClose(false);
+    }
+
+    const Fullscreen=()=>{
+        console.log("fullscreen");
+        onFullscreen(true);
+    }
+
+    const Window=()=>{
+        console.log("windowed");
+        onFullscreen(false);
+        onWindow(true);
+    }
     return(
         <div className="w-full h-full bg-background border border-muted-border flex flex-col">
-            <div className="flex flex-row items-center justify-center">
-                <h1>
+            <div className="flex flex-row items-center justify-center bg-foreground w-full h-[40px] p-2 gap-2">
+                <h1>    
                     {title}
                 </h1>
                 <div className="grow"/>
                 <div>
                     <Minimize2/>
                 </div>
-                <div>
+                <div className="cursor-pointer flex items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]" onClick={Window}>
                     <Square/>
                 </div>
-                <div>
+                <div className="cursor-pointer flex items-center justify-center hover:bg-red-300 rounded h-full w-[30px]" onClick={Terminate}>
                     <X/>
                 </div>
             </div>
