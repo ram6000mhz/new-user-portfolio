2c19d68a099b01cbc88a16ace1a0b00e912e07ae
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-11 19:13:02 +0800

    add double click to create portal and update to use callbacks for managing use state of portals

--- user-portfolio/src/apps/IconComponent.jsx
+++ user-portfolio/src/apps/IconComponent.jsx
@@ -8,14 +8,33 @@ export const IconComponent = ({Children, Title, isDragging}) => {
     const [isFullscreen, setIsFullscreen] = useState(false);
     const [isWindowed, setIsWindowed] = useState(false);
     const [isMinimized, setIsMinimized] = useState(false);
+
     const handleClick = () => {
         setIsFullscreen(true);
         setIsOpen(true);
     };
+
+    const killProcess = () => {
+        setIsOpen(false);
+        setIsFullscreen(false);
+        setIsWindowed(false);
+
+    }
+
+    const FullscreenMode = () => {
+        setIsFullscreen(!isFullscreen);
+        setIsWindowed(false);
+    }
+
+    const WindowMode = () => {
+        setIsFullscreen(!isFullscreen);
+        setIsWindowed(!isWindowed);
+    }
+
     return (
         <>
             <div className={`flex flex-col items-center justify-center h-[60px] w-[75px] p-1 rounded-md ${isDragging ? '' : 'hover:bg-foreground-highlight'}`}>
-                <div className="flex w-full h-full cursor-pointer items-center justify-center" onClick={handleClick}>
+                <div className="flex w-full h-full cursor-pointer items-center justify-center" onDoubleClick={handleClick}>
                     {Children}
                 </div>  
                 <p className="text-center text-xs text-accent-text cursor-pointer">{Title}</p>
@@ -26,23 +45,22 @@ export const IconComponent = ({Children, Title, isDragging}) => {
                         <div className="fixed inset-0 z-50 flex items-center justify-center bg-background h-screen w-screen">
                             <WindowComponent 
                                 title={Title}
-                                onClose={setIsOpen}
-                                onFullscreen={setIsFullscreen}
-                                isMinimize={setIsMinimized}
-                                onWindow={setIsWindowed}
+                                terminationcallback={killProcess}
+                                fullscreencallback={FullscreenMode}
+                                windowcallback={WindowMode}
                             />
                         </div>
                     )}
                     {isWindowed &&(
-                        <Rnd>
-                            <div className="fixed inset-0 z-50 flex items-center justify-center bg-background h-[100px]] w-[300px]">
+                        <Rnd 
+                            enableResizing={true}
+                        >
+                            <div className="fixed inset-0 z-50 flex items-center justify-center bg-background border-2 border-muted-border h-[200px] w-[300px]">
                                 <WindowComponent 
                                     title={Title}
-                                    onClose={setIsOpen}
-                                    onFullscreen={setIsFullscreen}
-                                    onWindow={setIsWindowed}
-                                    isMinimize={setIsMinimized}
-                                    
+                                    terminationcallback={killProcess}
+                                    fullscreencallback={FullscreenMode}
+                                    windowcallback={WindowMode}
                                 />
                             </div>
                         </Rnd>
