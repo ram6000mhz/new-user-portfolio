7af666be7e19ca03b12bfcd614621dd8a220cd46
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-12 15:29:26 +0800

    add fullscreen and windowed icon that appears conditionally

--- user-portfolio/src/components/WindowComponent.jsx
+++ user-portfolio/src/components/WindowComponent.jsx
@@ -1,26 +1,29 @@
-import {X, Square, Minimize2} from "lucide-react";
-export const WindowComponent=({title, children, terminationcallback,fullscreencallback, windowcallback})=>{
+import { Minus,X, Square, PictureInPicture2} from "lucide-react";
+export const WindowComponent=({title, children, isFullscreen, terminationcallback, windowcallback})=>{
+
     const Terminate=()=>{
         terminationcallback();
     }
+
     const Window=()=>{
         windowcallback();
     }
+
     return(
         <div className="w-full h-full bg-background flex flex-col">
-            <div className="flex flex-row items-center justify-center bg-foreground w-full h-[40px] p-2 gap-2">
-                <h1>    
+            <div className="flex flex-row items-center justify-center bg-foreground border-b-2 border-muted-border w-full h-[40px] p-2 gap-1 @container">
+                <h1 className="text-[10px] @sm:text-xs @md:text-sm @lg:text-base @xl:text-lg">   
                     {title}
                 </h1>
                 <div className="grow"/>
-                <div className="cursor-pointer flex items-center justify-center hover:bg-foreground-highlight rounded h-full w-[25px]">
-                    <Minimize2/>
+                <div className="cursor-pointer flex flex-col items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]">
+                    <Minus className="!h-[15px] !w-[15px]"/>
                 </div>
-                <div className="cursor-pointer flex items-center justify-center hover:bg-foreground-highlight rounded h-full w-[25px]" onClick={Window}>
-                    <Square/>
+                <div className="cursor-pointer flex items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]" onClick={Window}>
+                    {isFullscreen ? <PictureInPicture2 className="!h-[15px] !w-[15px] rotate-180 scale-y-[-1]"/>: <Square className="!h-[15px] !w-[15px]"/>}
                 </div>
-                <div className="cursor-pointer flex items-center justify-center hover:bg-red-300 rounded h-full w-[25px]" onClick={Terminate}>
-                    <X/>
+                <div className="cursor-pointer flex items-center justify-center hover:bg-red-300 rounded h-full w-[30px]" onClick={Terminate}>
+                    <X className="!h-[15px] !w-[15px]"/>
                 </div>
             </div>
             <div className="bg-amber-300 h-full w-full"></div>
