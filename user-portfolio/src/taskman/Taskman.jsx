f2e00f22e6b5cab58e30476061b64187b14b9aee
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-12 23:16:56 +0800

    add taskman provider

--- user-portfolio/src/taskman/Taskman.jsx
+++ user-portfolio/src/taskman/Taskman.jsx
@@ -1,2 +1,30 @@
 
-export const Taskman = [];
+import { createContext, useState, useContext } from "react";
+
+const TaskmanContext = createContext();
+
+export const TaskmanProvider = ( {children} ) => {
+    const [taskman, setTaskman] = useState([]);
+
+    const addTask = (taskIndex) => {
+        setTaskman((prevTasks) => [...prevTasks, taskIndex]);
+    }
+
+    const TerminateProcess = (taskIndex) => {
+        setTaskman((prevTasks) => prevTasks.filter((task => task.id !== taskIndex)))
+    }
+
+    return(
+        <TaskmanContext.Provider value={{taskman, addTask, TerminateProcess}}>
+            {children}
+        </TaskmanContext.Provider>
+    );
+}
+
+export const useTaskman = () =>{
+    const context = useContext(TaskmanContext);
+    if(!context){
+        throw new Error("useTaskman must be used within a TaskmanProvider");
+    }
+    return context;
+}
