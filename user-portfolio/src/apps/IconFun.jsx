d20e0b0b128cff98836903c5c043915434e0ea74
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-15 00:16:35 +0800

    fix icon component state issue 2

--- user-portfolio/src/apps/IconFun.jsx
+++ user-portfolio/src/apps/IconFun.jsx
@@ -16,48 +16,64 @@ export const IconFun = ({children}) => {
     };
 
     const updateAppState = (appIndex, updates) =>{
-        setAppStates(prevStates => ({
-            ...prevStates,
-            [appIndex]: {
-                ...getAppState(appIndex),
-                ...updates,
-            },
-        }));
+        setAppStates(prevStates => {
+            const newStates = {
+                ...prevStates,
+                [appIndex]: {
+                    ...prevStates[appIndex],
+                    ...updates,
+                },
+            };
+            console.log("Updated app:", appIndex, newStates[appIndex]);
+            return newStates;
+        });
     }
 
     const handleClick = (appIndex) => {
+        console.log("Icon double clicked:", appIndex);
         updateAppState(appIndex, {
+            isOpen: true,
             isFullscreen: true,
-            isOpen: true
+            isWindowed: false,
+            isMinimized: false,
         });
+        console.log("initial")
         console.log("Add to taskman:", appIndex);
         addTask(appIndex);
         console.log(taskman);
     };
 
     const killProcess = (appIndex) => {
+        console.log("Kill process:", appIndex);
         updateAppState(appIndex, {
             isOpen: false,
             isFullscreen: false,
-            isWindowed: false
+            isWindowed: false,
+            isMinimized: false
         });
         TerminateProcess(appIndex);
     }
 
     const WindowMode = (appIndex) => {
+        console.log("Window clicked");
         const currentState = getAppState(appIndex);
+        
         updateAppState(appIndex, {
+            isOpen: currentState.isOpen,
             isFullscreen: !currentState.isFullscreen,
-            isWindowed: !currentState.isWindowed
+            isWindowed: !currentState.isWindowed,
+            isMinimized: currentState.isMinimized  
         });
     }
 
     const MinimizeMode = (appIndex) => {
+        console.log("Minimize clicked");
         const currentState = getAppState(appIndex);
         updateAppState(appIndex, {
             isOpen: !currentState.isOpen,
-            isFullscreen: !currentState.isFullscreen,
-            isWindowed: !currentState.isWindowed
+            isFullscreen: currentState.isFullscreen,
+            isWindowed: currentState.isWindowed,
+            isMinimized: currentState.isMinimized
         });
     }
 
