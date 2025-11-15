d0d82425dfa094ddb4ac8ca69dc4ebe47f45b956
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-15 17:34:50 +0800

    add appid field

--- user-portfolio/src/apps/Applist.jsx
+++ user-portfolio/src/apps/Applist.jsx
@@ -2,10 +2,12 @@ import { Folder, User } from "lucide-react";
 
 export const apps = [
     { 
+        appid: 0,
         icon: <User className="!h-[25px] !w-[25px] accent-accent-icon"/>, 
         title: "About Me" 
     },
     { 
+        appid: 1,
         icon: <Folder className="!h-[25px] !w-[25px] accent-accent-icon"/>, 
         title: "My Projects" 
     },
