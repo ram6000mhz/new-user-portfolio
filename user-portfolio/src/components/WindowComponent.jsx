4f812a105d380f0a968c7af341f7faad7da7aeac
ram6000mhz <qesjyap@tip.edu.ph>
2025-11-21 17:42:36 +0800

    add color css to text and icons

--- user-portfolio/src/components/WindowComponent.jsx
+++ user-portfolio/src/components/WindowComponent.jsx
@@ -16,18 +16,18 @@ export const WindowComponent=({title, isFullscreen, terminationcallback, windowc
     return(
         <div className="w-full h-full bg-background flex flex-col">
             <div className="flex flex-row items-center justify-center bg-foreground border-b-2 border-muted-border w-full h-[40px] p-2 gap-1 @container">
-                <h1 className="text-[10px] @sm:text-xs @md:text-sm @lg:text-base @xl:text-lg">   
+                <h1 className="text-[10px] @sm:text-xs @md:text-sm @lg:text-base @xl:text-lg text-accent-text">   
                     {title}
                 </h1>
                 <div className="grow"/>
                 <div className="cursor-pointer flex flex-col items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]" onClick={Minimize} onTouchEnd={(e)=>{e.preventDefault();Minimize();}}>
-                    <Minus className="!h-[15px] !w-[15px]"/>
+                    <Minus className="!h-[15px] !w-[15px] text-accent-icon"/>
                 </div>
                 <div className="cursor-pointer flex items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]" onClick={Window} onTouchEnd={(e)=>{e.preventDefault();Window();}}>
-                    {isFullscreen ? <PictureInPicture2 className="!h-[15px] !w-[15px] rotate-180 scale-y-[-1]"/>: <Square className="!h-[15px] !w-[15px]"/>}
+                    {isFullscreen ? <PictureInPicture2 className="!h-[15px] !w-[15px] rotate-180 scale-y-[-1] text-accent-icon"/>: <Square className="!h-[15px] !w-[15px] text-accent-icon"/>}
                 </div>
                 <div className="cursor-pointer flex items-center justify-center hover:bg-red-300 rounded h-full w-[30px]" onClick={Terminate} onTouchEnd={(e)=>{e.preventDefault();Terminate();}}>
-                    <X className="!h-[15px] !w-[15px]"/>
+                    <X className="!h-[15px] !w-[15px] text-accent-icon"/>
                 </div>
             </div>
             {content}
