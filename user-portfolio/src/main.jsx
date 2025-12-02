180c464b86e5311242832612a74cd036bc4c7d98
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-02 13:55:33 +0800

    add prerendering

--- user-portfolio/src/main.jsx
+++ user-portfolio/src/main.jsx
@@ -1,5 +1,11 @@
-import { render } from 'preact'
+import { hydrate, prerender as ssr } from 'preact-iso';
 import './index.css'
 import { App } from './App.jsx'
 
-render(<App />, document.getElementById('app'))
+if (typeof window !== 'undefined') {
+    hydrate(<App />, document.getElementById('app'));
+}
+
+export async function prerender(data) {
+    return await ssr(<App {...data} />)
+}
