3ef8d0fb84b069be2b68bef78ae3163d3cdafa3a
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-02 13:54:55 +0800

    add prerendering

--- user-portfolio/vite.config.js
+++ user-portfolio/vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(),tailwindcss(),],
+  plugins: [
+    preact({
+      prerender:true,
+    }
+    ),tailwindcss(),],
   
 })
