e1f21d32b2f7b78b905e9275e4a8f5caa373fc8c
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-03 19:37:59 +0800

    add lazyloading

--- user-portfolio/src/pages/Machineviewport.jsx
+++ user-portfolio/src/pages/Machineviewport.jsx
@@ -1,8 +1,11 @@
 import { Rnd } from "react-rnd";
-import { IconComponent } from "../apps/IconComponent";
+import { lazy } from "preact-iso";
 import { apps } from "../apps/Applist";
+import { Suspense } from "preact/compat";
 import { useState, useEffect } from "react";
 import DesktopBg from "../assets/img/desktop-bg.webp"
+
+const IconComponent = lazy(()=>import("../apps/IconComponent").then(m=>({default:m.IconComponent})));
 
 export const Machineviewport=()=>{
     const [isDragging, setIsDragging] = useState(false);
@@ -11,25 +14,27 @@ export const Machineviewport=()=>{
         <div className="w-full h-full bg-cover bg-center flex flex-col relative" style={{ backgroundImage: `url(${DesktopBg})` }}>
             
                 {apps.map((app, index) => (
-                    <Rnd 
-                        onDragStart={() => {
-                            setIsDragging(true);
-                        }}
-                        
-                        onDragStop={() => {
-                            setIsDragging(false);
-                        }}
-                        
-                        key={index}
-                        bounds="parent"
-                        enableResizing={false}
-                        default={{
-                            x: 0,
-                            y: index * 60,
-                        }}
-                    >
-                        <IconComponent Children={app.icon} Title={app.title} appContent={app.content} isDragging={ isDragging } appIndex={app.appid}/>
-                    </Rnd>
+                    <Suspense fallback={null}>
+                        <Rnd 
+                            onDragStart={() => {
+                                setIsDragging(true);
+                            }}
+                            
+                            onDragStop={() => {
+                                setIsDragging(false);
+                            }}
+                            
+                            key={index}
+                            bounds="parent"
+                            enableResizing={false}
+                            default={{
+                                x: 0,
+                                y: index * 60,
+                            }}
+                        >
+                            <IconComponent Children={app.icon} Title={app.title} appContent={app.content} isDragging={ isDragging } appIndex={app.appid}/>
+                        </Rnd>
+                    </Suspense>
                 ))}
             
         </div>
