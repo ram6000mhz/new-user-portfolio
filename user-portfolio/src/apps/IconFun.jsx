90f7890f054a1dc4683804e7944c16c6d9d991e1
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-14 20:04:06 +0800

    first working zustand version of iconfun provider

--- user-portfolio/src/apps/IconFun.jsx
+++ user-portfolio/src/apps/IconFun.jsx
@@ -1,37 +1,66 @@
 import { create } from "zustand";
 
-export const useIconStore = () =>
-  create(set => ({
-    isOpen: false,
-    isFullscreen: false,
-    isWindowed: false,
-    isMinimized: false,
+export const IconFun = create((set, get) => ({
+  appStates: {}, 
 
-    open: () =>
-      set({
-        isOpen: true,
-        isFullscreen: true,
-        isWindowed: false,
-        isMinimized: false,
-      }),
+  initializeApp: (id, initialState) =>
+    set(s => ({
+      appStates: {
+        ...s.appStates,
+        [id]: initialState || {
+          isDragging: false,
+          isOpen: false,
+          isFullscreen: false,
+          isWindowed: false,
+          isMinimized: false,
+        },
+      },
+    })),
 
-    kill: () =>
-      set({
-        isOpen: false,
-        isFullscreen: false,
-        isWindowed: false,
-        isMinimized: false,
-      }),
+  setAppState: (id, newState) => {
+   if (!get().appStates[id]) return; 
 
-    toggleWindow: () =>
-      set(s => ({
-        isFullscreen: !s.isFullscreen,
-        isWindowed: !s.isWindowed,
-      })),
+    set(s => ({
+      appStates: {
+        ...s.appStates,
+        [id]: {
+          ...s.appStates[id],
+          ...newState,
+        },
+      },
+    }));
+  },
 
-    toggleMinimize: () =>
-      set(s => ({
-        isOpen: !s.isOpen,
-        isMinimized: !s.isMinimized,
-      })),
-  }));
+  setDragging: (id, dragging) => {
+    console.log(`Setting app ID ${id} isDragging to ${dragging}`);
+    get().setAppState(id, { isDragging: dragging });
+  },
+  
+  open: (id) => {
+    get().setAppState(id, {
+      isOpen: true,
+      isFullscreen: true,
+      isWindowed: false,
+      isMinimized: false,
+    });
+  },
+  kill: () =>
+    set({
+      isOpen: false,
+      isFullscreen: false,
+      isWindowed: false,
+      isMinimized: false,
+    }),
+
+  toggleWindow: () =>
+    set(s => ({
+      isFullscreen: !s.isFullscreen,
+      isWindowed: !s.isWindowed,
+    })),
+
+  toggleMinimize: () =>
+    set(s => ({
+      isOpen: !s.isOpen,
+      isMinimized: !s.isMinimized,
+    })),
+}));
