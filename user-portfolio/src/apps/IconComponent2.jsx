0babca04fbec6a426d408074571f9e9a4b0f11a4
ram6000mhz <139690904+ram6000mhz@users.noreply.github.com>
2025-12-14 20:07:31 +0800

    Revert "add state for call from zustand for isdragging"
    This reverts commit 2ff2b8d8410d20624d69489f8699c26f1abd1633.

--- user-portfolio/src/apps/IconComponent2.jsx
+++ user-portfolio/src/apps/IconComponent2.jsx
@@ -4,11 +4,24 @@ import { createPortal } from "react-dom";
 import { Rnd } from "react-rnd";
 import { useZIndexShuffler } from "../providers/ZIndexShuffler";
 import { AnimatePresence, motion } from "motion/react";
-import { IconFun } from "../apps/IconFun";
+import { useIconStore } from "../apps/IconFun";
 
-export const IconComponent2 = ({AppIcon, Title, appIndex}) => {
+export const IconComponent2 = ({AppIcon, Title, appContent, isDragging, appIndex}) => {
     console.log("IconComponent rendered");
-    const isDragging = IconFun(s => s.appStates[appIndex]?.isDragging || false);
+    const isOpen = useIconStore(
+        s => s.appStates[appIndex]?.isOpen ?? false
+    );
+    const isFullscreen = useIconStore(
+        s => s.appStates[appIndex]?.isFullscreen ?? false
+    );
+
+    const handleClick = useIconStore(s => s.handleClick);
+    const killProcess = useIconStore(s => s.killProcess);
+    const WindowMode = useIconStore(s => s.WindowMode);
+    const MinimizeMode = useIconStore(s => s.MinimizeMode);
+
+    const { zMap, bringToFront } = useZIndexShuffler();
+
     return (
         <>
             <div className={`flex flex-col items-center justify-center h-[60px] w-[75px] p-1 rounded-md ${isDragging ? '' : 'hover:bg-foreground-highlight'}`}>
