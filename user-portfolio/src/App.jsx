80f5c79aae86dc71a2cb544c317f8eee1b239ea5
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-15 21:59:13 +0800

    remove old zindex provider call

--- user-portfolio/src/App.jsx
+++ user-portfolio/src/App.jsx
@@ -2,7 +2,6 @@ import { Router, Route, lazy, prerender, LocationProvider } from 'preact-iso';
 import { MainLayout } from './pages/MainLayout';
 import { Machineviewport } from './pages/Machineviewport';
 import { TaskmanProvider } from './taskman/Taskman';
-import { ZIndexShufflerProvider } from './providers/ZIndexShuffler';
 
 function HomePage() {
   return (
@@ -16,13 +15,11 @@ export function App() {
   console.log("App rendered");
   return (
     <TaskmanProvider>
-      <ZIndexShufflerProvider>
           <div className='w-screen h-screen'>
             <LocationProvider>
                 <HomePage path="/" />
             </LocationProvider>
           </div>
-      </ZIndexShufflerProvider>
     </TaskmanProvider>
   )
 }
