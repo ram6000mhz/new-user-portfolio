7354f2fc3e5a0118bec7c1e86609d7d117b31bbc
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-15 22:11:04 +0800

    backup old version of zindex shuffler

---
+++ user-portfolio/src/providers/ZIndexShufflerold.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const ZIndexShufflerContext = createContext();
+
+export const ZIndexShufflerProvider = ( {children} ) => {
+    const [zMap, setZmap] = useState({dummyobject: 0});
+    const [maxZ, setMaxZ] = useState(0);
+
+    useEffect(() => {
+    }, [zMap, maxZ]);
+
+    const bringToFront = (appIndex) => {
+
+        const entries = Object.entries(zMap);
+        const max = entries.length - 1;
+
+        const newMap = {};
+        for (const [k, v] of entries) {
+            newMap[k] = v > zMap[appIndex] ? v - 1 : v;
+        }
+        newMap[appIndex] = max;
+
+        setZmap(newMap);
+    };
+
+    const removeZIndexOnTerminate = (appIndex) => {
+
+    };
+
+    return(
+        <ZIndexShufflerContext.Provider value={{zMap, bringToFront}}>
+            {children}
+        </ZIndexShufflerContext.Provider>
+    )
+}
+
+export const useZIndexShuffler = () =>{
+    return useContext(ZIndexShufflerContext);
+}
