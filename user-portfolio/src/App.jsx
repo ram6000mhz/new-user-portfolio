57cf6d1ba26c363512e6e2447fbc07f49948bdcd
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-17 17:57:17 +0800

    remove old taskman provider wrapper

--- user-portfolio/src/App.jsx
+++ user-portfolio/src/App.jsx
@@ -1,7 +1,6 @@
 import { Router, Route, lazy, prerender, LocationProvider } from 'preact-iso';
 import { MainLayout } from './pages/MainLayout';
 import { Machineviewport } from './pages/Machineviewport';
-import { TaskmanProvider } from './taskman/Taskman';
 
 function HomePage() {
   return (
@@ -14,12 +13,10 @@ function HomePage() {
 export function App() {
   console.log("App rendered");
   return (
-    <TaskmanProvider>
-          <div className='w-screen h-screen'>
-            <LocationProvider>
-                <HomePage path="/" />
-            </LocationProvider>
-          </div>
-    </TaskmanProvider>
+    <div className='w-screen h-screen'>
+      <LocationProvider>
+          <HomePage path="/" />
+      </LocationProvider>
+    </div>
   )
 }
