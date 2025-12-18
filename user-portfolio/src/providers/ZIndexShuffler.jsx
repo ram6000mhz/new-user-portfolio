33cdf751cbb2c17077cf646a7d786d5a41510d53
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-18 21:25:38 +0800

    add new zindex shuffler made in zustand

--- user-portfolio/src/providers/ZIndexShuffler.jsx
+++ user-portfolio/src/providers/ZIndexShuffler.jsx
@@ -2,54 +2,32 @@ import {create}  from 'zustand';
 
 export const ZIndexShuffler = create(( set, get ) => ({
     zMap: {dummyobject: 0 },
-    bringToFront: (appIndex) => set((state) => {
-        const { zMap } = state;
-        const currentZ = zMap[appIndex];
-        const entries = Object.entries(zMap);
-        const maxZ = entries.length - 1;
+    bringToFront: ((appId) => {
+        set((state) => {
+            const entries = Object.entries(state.zMap);
+            const max = entries.length - 1;
+            const targetZ = state.zMap[appId];
 
-        if (currentZ === maxZ) {
-            return state; 
-        }
+            if (targetZ === max) return state;
 
-        const newMap = {};
-        for (const [k, v] of entries) {
-            if (k === appIndex) {
-                newMap[k] = maxZ;
-            } 
-            else if (v > currentZ) {
-                newMap[k] = v - 1;
+            const newMap = {};
+            for (const [k, v] of entries) {
+                newMap[k] = v > targetZ ? v - 1 : v;
             }
-            else {
-                newMap[k] = v;
-            }
-        }
-        console.log(get().zMap)
-        return { zMap: newMap };
+            newMap[appId] = max;
+            return { zMap: newMap };
+        })
     }),
 
-    removeZIndexOnTerminate: (appIndex) => set((state) => {
-        const { zMap } = state;
+    removeAppZmap: (appId) =>
+        set((state) => {
+        const { [appId]: removedZ, ...remainingMap } = state.zMap;
+        if (removedZ === undefined) return state;
 
-        if (!(appIndex in zMap)) {
-            return state;
-        }
-
-        const zToRemove = zMap[appIndex];
         const newMap = {};
-        
-        for (const [k, v] of Object.entries(zMap)) {
-            if (k === appIndex) {
-                continue;
-            }
-            if (v > zToRemove) {
-                newMap[k] = v - 1;
-            } 
-            else {
-                newMap[k] = v;
-            }
+        for (const [k, v] of Object.entries(remainingMap)) {
+            newMap[k] = v > removedZ ? v - 1 : v;
         }
-
         return { zMap: newMap };
-    }),
+        }),
 }))
