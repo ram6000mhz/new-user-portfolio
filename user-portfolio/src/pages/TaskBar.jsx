512ca7f9c9d601fadc3641ae53f5334677c74f84
ram6000mhz <qesjyap@tip.edu.ph>
2025-12-27 14:19:51 +0800

    create new seperate file for taskbar code

---
+++ user-portfolio/src/pages/TaskBar.jsx
@@ -0,0 +1,34 @@
+import { apps } from "../apps/Applist";
+import { Taskman } from "../taskman/Taskman";
+import {IconFun} from "../apps/IconFun"
+
+export const TaskBar = () =>{
+    console.log("TaskBar rendered");
+
+    const taskman = Taskman((state) => state.taskman);
+    const { taskBarOpenClose } = IconFun.getState();
+
+    const activeApps = apps.filter(app => taskman.includes(app.appid));
+
+    const reOpenWindow = (appId) => {
+        taskBarOpenClose(appId);
+    }
+
+    return (
+        <>
+            {activeApps.map((app, index) => (
+            <div className="flex items-center justify-center 
+            h-[35px] w-[35px] hover:bg-foreground-highlight 
+            rounded cursor-pointer" 
+            onClick={
+                ()=>reOpenWindow(app.appid)} 
+                onTouchEnd={(e)=>{
+                    e.preventDefault();
+                    reOpenWindow(app.appid);
+                }}>
+                {app.icon}
+            </div>
+            ))}
+        </>
+    )
+}
