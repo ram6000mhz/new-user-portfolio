c2f2c7b6b7749bfea68fb49d8bdd9c07204bdf35
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-06 20:08:35 +0800

    add new layout for 4 relevant information items

--- user-portfolio/src/apps/appcontent/Aboutme.jsx
+++ user-portfolio/src/apps/appcontent/Aboutme.jsx
@@ -1,43 +1,59 @@
-import {Power, Linkedin, Mail, Github} from "lucide-react";
+import {Power, Linkedin, Mail, Github, MapPin} from "lucide-react";
 
 export const Aboutme = ()=>{
     return(
-        <div className="flex flex-col items-start w-full h-full bg-white relative px-80 py-5 @container">
+        <div className="flex flex-col items-start w-full h-full bg-white relative px-80 pt-5 @container gap-3">
             <h1 className="font-bold text-5xl">
                 Ethan Sancho Yap
             </h1>
-            <h2 className="font-semibold text-xl py-2 text-muted-text">
+            <h2 className="font-semibold text-xl text-muted-text">
                 Software developer
             </h2>
-            <h3 className="font-extralight text-md py-4">
-                small description
+            <h3 className="font-extralight text-md">
+                I enjoy building interesting projects.
             </h3>
-            <div className="w-full h-px bg-muted-text my-6" />
-            <div className="grid grid-cols-2 w-full">
-                <div className="flex flex-col items-start justify-center border-muted-border">
-                    <h2 className="font-semibold text-md py-2 text-muted-text">Contact</h2>
-                    <div className="flex flex-row justify-items-center gap-3">
-                        <div className="flex flex-row gap-2">
+            <div className="w-full h-px bg-muted-text my-5" />
+            <div className="w-full">
+                <div className="flex flex-row items-center justify-center w-full gap-5">
+                    <div className="flex flex-col gap-1">
+                        <h2 className="font-semibold text-sm text-muted-text">Email</h2>
+                        <div className="flex flex-row gap-1 justify-center items-center">
                             <Mail className="!w-[35px] !h-[35px] text-black"/>
-                            <h4 className="font-sm text-sm text-muted-text">
+                            <h4 className="font-sm text-sm text-black">
                                 esyworkpro@gmail.com
                             </h4>
                         </div>
-                        <div className="flex flex-row gap-2">
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h2 className="font-semibold text-sm text-muted-text">Github</h2>
+                        <div className="flex flex-row gap-1 justify-center items-center">
                             <Github className="!w-[35px] !h-[35px] text-black"/>
-                            <h4 className="font-sm text-sm text-muted-text">
-                                Github
+                            <h4 className="font-sm text-sm text-black">
+                                github.com/ram6000mhz
                             </h4>
                         </div>
-                        <div className="flex flex-row gap-2">
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h2 className="font-semibold text-sm text-muted-text">LinkedIn</h2>
+                        <div className="flex flex-row gap-1 justify-center items-center">
                             <Linkedin className="!w-[35px] !h-[35px] text-black"/>
-                            <h4 className="font-sm text-sm text-muted-text">
-                                LinkdIn
+                            <h4 className="font-sm text-sm text-black">
+                                www.linkedin.com/in/ethan-sancho-yap-2439a2297/
+                            </h4>
+                        </div>
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <h2 className="font-semibold text-sm text-muted-text">Location</h2>
+                        <div className="flex flex-row gap-1 justify-center items-center">
+                            <MapPin className="!w-[35px] !h-[35px] text-black"/>
+                            <h4 className="font-sm text-sm text-black">
+                                Philippines
                             </h4>
                         </div>
                     </div>
                 </div>
             </div>
+            <div className="w-full h-px bg-muted-text my-6" />
         </div>
     )
 }
