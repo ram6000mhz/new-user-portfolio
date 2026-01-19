54a4aed16657244f0bfcd7a7bb423b45cf7fd6c6
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-20 01:05:15 +0800

    add desktop footer code

---
+++ user-portfolio/src/pages/DesktopFooter.jsx
@@ -0,0 +1,31 @@
+import { ViewHandler } from "../providers/ViewHandler";
+import {BuildStartWindow} from "./BuildStartWindow"
+import {TaskBar} from "./Taskbar"
+export const DesktopFooter = () => {
+    const hr_mode = ViewHandler((state) => state.hr_Mode);
+    
+    const now = new Date().toLocaleString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true,
+    });
+
+    const date = new Date().toLocaleDateString();
+
+    return (
+        <>
+        {!hr_mode &&(
+            <footer className="w-full h-[50px] flex flex-row items-center border-t-2 border-muted-border p-2 sm:p-2 md:p-4 lg:p-6 bg-foreground z-50">
+                <BuildStartWindow/>
+                <TaskBar/>
+                <div className="grow"></div>
+                <div className="flex flex-col items-center justify-center h-full">
+                    <time className="text-xs sm:text-sm text-accent-text">{now}</time>
+                    <time className="text-xs sm:text-sm text-accent-text">{date}</time>
+                </div>
+            </footer>
+            )
+        }
+      </>
+    )
+}
