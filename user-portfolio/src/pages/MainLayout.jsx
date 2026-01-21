5d18e8c1ee2561dad377641065d67af0c174b99f
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-21 16:29:22 +0800

    add footer seperation

--- user-portfolio/src/pages/MainLayout.jsx
+++ user-portfolio/src/pages/MainLayout.jsx
@@ -2,20 +2,15 @@ import { Machineviewport } from './Machineviewport';
 import { ViewHandler } from "../providers/ViewHandler";
 import { Aboutme } from "../apps/appcontent/Aboutme";
 import { DesktopFooter } from './DesktopFooter';
+import {Navbar} from './Navbar'
 // text-xs sm:text-sm md:text-base lg:text-lg
 export const MainLayout = () => {
   console.log("MainLayout rendered");
-  const toggleHrMode = ViewHandler((state) => state.toggleHrMode);
   const hr_mode = ViewHandler((state) => state.hr_Mode);
 
   return (
     <div className="w-full h-full flex flex-col">
-      {hr_mode &&( 
-        <nav 
-            className="h-[40px] w-full bg-amber-200" 
-            onClick={toggleHrMode}>
-        </nav> 
-      )}
+      <Navbar/>
       <main className="grow">
         {hr_mode ? <Aboutme/>:<Machineviewport/>}
       </main>
