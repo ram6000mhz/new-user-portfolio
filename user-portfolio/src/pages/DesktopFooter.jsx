bf54a5f32f747f22d0495e3cec75378bcf2cd7a5
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-23 19:16:58 +0800

    remove in component timer and add new seperated rerendering timer component

--- user-portfolio/src/pages/DesktopFooter.jsx
+++ user-portfolio/src/pages/DesktopFooter.jsx
@@ -1,25 +1,16 @@
 import { ViewHandler } from "../providers/ViewHandler";
 import {BuildStartWindow} from "./BuildStartWindow"
 import {TaskBar} from "./Taskbar"
+import { Timer } from "./Timer";
 export const DesktopFooter = () => {
     console.log("renderdesktopfooter")
-    const now = new Date().toLocaleString([], {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-    });
-
-    const date = new Date().toLocaleDateString();
 
     return (
         <footer className="w-full h-[50px] flex flex-row items-center border-t-2 border-muted-border p-2 sm:p-2 md:p-4 lg:p-6 bg-foreground z-50">
             <BuildStartWindow/>
             <TaskBar/>
             <div className="grow"></div>
-            <div className="flex flex-col items-center justify-center h-full">
-                <time className="text-xs sm:text-sm text-accent-text">{now}</time>
-                <time className="text-xs sm:text-sm text-accent-text">{date}</time>
-            </div>
+            <Timer/>
         </footer>
     )
 }
