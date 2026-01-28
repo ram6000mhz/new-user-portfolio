884fdf6b902f60d0c1a3f222da1b7d7ef4ce0c87
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-28 17:31:17 +0800

    add contacts code

---
+++ user-portfolio/src/apps/appcontent/aboutme/Contacables.jsx
@@ -0,0 +1,51 @@
+import {Power, Linkedin, Mail, Github, MapPin} from "lucide-react";
+export const Contacables = () => {
+    return(
+        <div className="w-full">
+            <div className="flex flex-col items-start @2xl:flex-row @2xl:items-center w-full gap-5 px-3">
+                <div className="flex flex-col gap-1">
+                    <h2 className="font-semibold text-sm text-muted-text">Email</h2>
+                    <div className="flex flex-row gap-1 justify-center items-center">
+                        <Mail className="!w-[35px] !h-[35px] text-black"/>
+                        <h4 className="font-sm text-sm text-black">
+                            esyworkpro@gmail.com
+                        </h4>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <h2 className="font-semibold text-sm text-muted-text">Github</h2>
+                    <div className="flex flex-row gap-1 justify-center items-center">
+                        <Github className="!w-[35px] !h-[35px] text-black"/>
+                        <h4 className="font-sm text-sm text-black">
+                            github.com/ram6000mhz
+                        </h4>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <h2 className="font-semibold text-sm text-muted-text">LinkedIn</h2>
+                    <div className="flex flex-row gap-1 justify-center items-center">
+                        <Linkedin className="!w-[35px] !h-[35px] text-black"/>
+                        <a 
+                            href="http://linkedin.com/in/ethan-sancho-yap-2439a2297/"
+                            target="_blank" 
+                            rel="noopener noreferrer"
+                        >
+                            <h4 className="font-sm text-sm text-black">
+                                linkedin.com/in/ethan-sancho-yap
+                            </h4>
+                        </a>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <h2 className="font-semibold text-sm text-muted-text">Location</h2>
+                    <div className="flex flex-row gap-1 justify-center items-center">
+                        <MapPin className="!w-[35px] !h-[35px] text-black"/>
+                        <h4 className="font-sm text-sm text-black"> 
+                            Philippines
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
