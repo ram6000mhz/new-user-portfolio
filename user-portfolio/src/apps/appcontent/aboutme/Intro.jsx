d12822e5a3201355b78270ce46e14a9e06cea93b
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-28 17:31:53 +0800

    add intro code

---
+++ user-portfolio/src/apps/appcontent/aboutme/Intro.jsx
@@ -0,0 +1,15 @@
+export const Intro = () =>{
+    return(
+        <>
+            <h1 className="font-bold text-5xl">
+                Ethan Sancho Yap
+            </h1>
+            <h2 className="font-semibold text-xl text-muted-text">
+                Software developer
+            </h2>
+            <h3 className="font-extralight text-md">
+                I enjoy building interesting projects.
+            </h3>
+        </>
+    )
+}
