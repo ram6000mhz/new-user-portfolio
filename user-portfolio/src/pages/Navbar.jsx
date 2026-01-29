496d01d70389f6ab6d9b86a1db74719facbafe99
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-29 18:58:24 +0800

    src/pages/Navbar.jsx

--- user-portfolio/src/pages/Navbar.jsx
+++ user-portfolio/src/pages/Navbar.jsx
@@ -1,10 +1,10 @@
 import { ViewHandler } from "../providers/ViewHandler";
+import {Power} from "lucide-react";
 export const Navbar=()=>{
     const toggleHrMode = ViewHandler((state) => state.toggleHrMode);
     return(
-        <nav 
-            className="h-[70px] w-full bg-muted-border" 
-            onClick={toggleHrMode}>
+        <nav className="flex flex-row h-[70px] w-full items-center justify-end bg-white border-b px-10">    
+            <Power onClick={toggleHrMode} className="!h-[25px] !w-[25px] text-black cursor-pointer"/>
         </nav> 
     )
 }
