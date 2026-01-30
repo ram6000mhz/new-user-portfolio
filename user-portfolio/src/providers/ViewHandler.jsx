aed1eae295028a6e5e13b199501b2b1e67573c9f
ram6000mhz <ethansanchoyap@gmail.com>
2026-01-30 17:05:32 +0800

    add shutdown code for state managers

--- user-portfolio/src/providers/ViewHandler.jsx
+++ user-portfolio/src/providers/ViewHandler.jsx
@@ -1,8 +1,19 @@
 import { create } from 'zustand';
+import  {IconFun } from '../apps/IconFun'
+import {ZIndexShuffler} from '../providers/ZIndexShuffler'
+import { Taskman } from '../taskman/Taskman';
 
-export const ViewHandler = create((set) => ({
+export const ViewHandler = create((set,get) => ({
     hr_Mode: true,
     toggleHrMode() {
         set((state) => ({ hr_Mode: !state.hr_Mode }));
+        if(!get().hr_Mode){
+            const { shutdownIconFun } = IconFun.getState();
+            const { shutdownTaskman } = Taskman.getState();
+            const { shutdownZIndexShuffler } = ZIndexShuffler.getState();
+            shutdownIconFun();
+            shutdownTaskman();
+            shutdownZIndexShuffler();
+        }
     },
 }))
