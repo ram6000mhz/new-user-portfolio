195b46be6f795a2c582560c57a97852bae793b76
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-01 19:31:45 +0800

    add lazy loading

--- user-portfolio/src/pages/MainLayout.jsx
+++ user-portfolio/src/pages/MainLayout.jsx
@@ -1,11 +1,20 @@
-import { Machineviewport } from './Machineviewport';
+import { lazy, Suspense } from 'preact/compat';
 import { ViewHandler } from "../providers/ViewHandler";
 import { Aboutme } from "../apps/appcontent/aboutme/Aboutme";
 import { DesktopFooter } from './DesktopFooter';
 import {Navbar} from './Navbar'
 // text-xs sm:text-sm md:text-base lg:text-lg
+
+const loaddesktop = () => import('./Machineviewport').then(m => m.Machineviewport)
+
+const Machineviewport = lazy(loaddesktop);
+
 export const MainLayout = () => {
   const hr_mode = ViewHandler((state) => state.hr_Mode);
+
+  if (hr_mode) {
+    loaddesktop();
+  }
 
   return (
     <div className="w-full h-full flex flex-col">
@@ -13,7 +22,9 @@ export const MainLayout = () => {
         <Navbar/>
       )}
       <main className="flex-1 min-h-0">
-        {hr_mode ? <Aboutme/>:<Machineviewport/>}
+        <Suspense fallback={null}>
+          {hr_mode ? <Aboutme /> : <Machineviewport />}
+        </Suspense>
       </main>
       {!hr_mode &&(
         <DesktopFooter/>
