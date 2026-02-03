ee99582cfe845dac397beac075b55db5accf2b9c
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-03 19:00:43 +0800

    preload desktopbg early

--- user-portfolio/src/pages/MainLayout.jsx
+++ user-portfolio/src/pages/MainLayout.jsx
@@ -1,8 +1,10 @@
 import { lazy, Suspense } from 'preact/compat';
+import { useEffect } from 'react';
 import { ViewHandler } from "../providers/ViewHandler";
 import { Aboutme } from "../apps/appcontent/aboutme/Aboutme";
 import { DesktopFooter } from './DesktopFooter';
 import {Navbar} from './Navbar'
+import DesktopBg from "../assets/img/rice.webp"
 // text-xs sm:text-sm md:text-base lg:text-lg
 
 const loaddesktop = () => import('./Machineviewport').then(m => m.Machineviewport)
@@ -12,9 +14,13 @@ const Machineviewport = lazy(loaddesktop);
 export const MainLayout = () => {
   const hr_mode = ViewHandler((state) => state.hr_Mode);
 
-  if (hr_mode) {
-    loaddesktop();
-  }
+  useEffect(() => {
+    if (hr_mode) {
+      loaddesktop();
+        const img = new Image();
+        img.src = DesktopBg;
+      }
+  }, [hr_mode]);
 
   return (
     <div className="w-full h-full flex flex-col">
