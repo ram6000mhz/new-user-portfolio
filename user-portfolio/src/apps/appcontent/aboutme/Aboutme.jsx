c3ed0d2821c9a72067bdd53d89b6fce1111ad764
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-06 18:24:17 +0800

    minor layout change to items

--- user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
@@ -6,7 +6,7 @@ export const Aboutme = ()=>{
     return(
         <div className="flex w-full h-full flex-row @container overflow-auto no-scrollbar bg-white">
             <div class="w-[10%] h-full"/>
-            <div className="flex-1 flex-col items-start h-full relative px-5 pt-5 gap-3">
+            <div className="flex-1 flex-col items-center h-full relative pt-5 gap-3">
                 <Intro/>
                 <Contacables/>
                 <Skillchart/>
