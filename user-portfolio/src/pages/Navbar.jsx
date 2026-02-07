b6264e2753c2fdf8c23d98cded9b5aa6a1d49312
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-07 11:25:28 +0800

    changes on navbar design

--- user-portfolio/src/pages/Navbar.jsx
+++ user-portfolio/src/pages/Navbar.jsx
@@ -3,7 +3,7 @@ import {Power} from "lucide-preact";
 export const Navbar=()=>{
     const toggleHrMode = ViewHandler((state) => state.toggleHrMode);
     return(
-        <nav className="flex flex-row h-[70px] w-full items-center justify-end bg-zinc-100 border-b border-zinc-300 px-10">    
+        <nav className=" flex flex-row h-[70px] w-full items-center justify-end px-10 fixed top-0 z-50 bg-white border-b border-muted-border">    
             <Power onClick={toggleHrMode} className="!h-[25px] !w-[25px] text-black cursor-pointer"/>
         </nav> 
     )
