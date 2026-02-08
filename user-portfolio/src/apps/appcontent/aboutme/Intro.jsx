a386ce4864a8b9b0587c500c57fee6d125245d36
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-08 09:37:38 +0800

    add 3d intro

--- user-portfolio/src/apps/appcontent/aboutme/Intro.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Intro.jsx
@@ -1,15 +1,22 @@
+import { Hero3d } from "./Hero3d"
+
 export const Intro = () =>{
     return(
-        <>
-            <h1 className="font-bold text-5xl">
-                Ethan Sancho Yap
-            </h1>
-            <h2 className="font-semibold text-xl text-muted-text">
-                Software developer
-            </h2>
-            <h3 className="font-extralight text-md">
-                I enjoy building interesting projects.
-            </h3>
-        </>
+        <div className="flex flex-row w-full h-full items-center justify-center">
+            <div className="bg-black flex-1 h-full grid place-content-center justify-items-start">
+                <h1 className="font-bold text-5xl text-white">
+                   Ethan Sancho Yap
+                </h1>
+                <h2 className="font-semibold text-xl text-muted-text">
+                    Software developer
+                </h2>
+                <h3 className="font-extralight text-md text-muted-text">
+                    I enjoy building interesting projects.
+                </h3>
+            </div>
+            <div className="bg-black flex-1 h-full ">
+                <Hero3d></Hero3d>
+            </div>
+        </div>
     )
 }
