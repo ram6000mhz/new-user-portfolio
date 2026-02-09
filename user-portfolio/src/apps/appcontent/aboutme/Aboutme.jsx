a4aa64d832a7b2b2b73795314bdf9174c05e6126
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-09 08:50:02 +0800

    remove contacables in about me layout

--- user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
@@ -1,5 +1,4 @@
 import { Skillchart } from "./Skillchart";
-import { Contacables } from "./Contacables";
 import { Intro } from "./Intro";
 
 export const Aboutme = ()=>{
@@ -7,7 +6,6 @@ export const Aboutme = ()=>{
         <div className="flex w-full h-full flex-row @container overflow-auto overscroll-none no-scrollbar bg-black">
             <div className="flex-1 flex-col items-center h-full relative">
                 <Intro/>
-                <Contacables/>
                 <Skillchart/>      
             </div>    
         </div> 
