7776bb850c619408de83ac4c1d9781371fe7da28
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-10 09:47:42 +0800

    lazy load skillchart

--- user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
@@ -1,12 +1,16 @@
-import { Skillchart } from "./Skillchart";
+import { lazy, Suspense } from 'preact/compat';
 import { Intro } from "./Intro";
+
+const Skillchart = lazy(() => import('./Skillchart').then(m => m.Skillchart));
 
 export const Aboutme = ()=>{
     return(
         <div className="flex w-full h-full flex-row @container overflow-auto overscroll-none no-scrollbar bg-black">
             <div className="flex-1 flex-col items-center h-full relative">
                 <Intro/>
-                <Skillchart/>      
+                <Suspense fallback={null}>
+                    <Skillchart/>   
+                </Suspense>   
             </div>    
         </div> 
     )
