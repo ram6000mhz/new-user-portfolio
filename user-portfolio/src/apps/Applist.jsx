215436a0ec86b3836bc9da7dcf3e984121c44eda
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-21 20:32:32 +0800

    redo applist

--- user-portfolio/src/apps/Applist.jsx
+++ user-portfolio/src/apps/Applist.jsx
@@ -17,51 +17,6 @@ export const apps = [
     },
     { 
         appid: 1,
-        icon: <ScanFace className="!h-[25px] !w-[25px] text-accent-icon"/>, 
-        title: "Opti-Face" ,
-        content: 
-        <div className="flex flex-col items-center">
-            A lightweight facial recognition application built by using scrfd, mediapipe's FaceMesh, and Edgeface that is still accurate and fast.
-        </div>
-    },
-    { 
-        appid: 2,
-        icon: <Soup className="!h-[25px] !w-[25px] text-accent-icon"/>, 
-        title: "Bake, Brew and Cook" ,
-        content: 
-        <div className="">
-            A small recipe application that contains a variety of Filipino recipes for baking, brewing, and cooking.
-        </div>
-    },
-    { 
-        appid: 3,
-        icon: <Gamepad2 className="!h-[25px] !w-[25px] text-accent-icon"/>, 
-        title: "Math Solve" ,
-        content: 
-        <div className="">
-            A game that generates a random -+/x problem to solve within a time limit.
-        </div>
-    },
-    { 
-        appid: 4,
-        icon: <BusFront className="!h-[25px] !w-[25px] text-accent-icon"/>, 
-        title: "Magic C Transport" ,
-        content: 
-        <div className="">
-            A transportation booking application for Magic C Transport Services.
-        </div>
-    },
-    { 
-        appid: 5,
-        icon: <BotMessageSquare className="!h-[25px] !w-[25px] text-accent-icon"/>, 
-        title: "T.I.P.A.I" ,
-        content: 
-        <div className="">
-            A simple AI chatbot that responds to user's queries about the technological institute of the philippines.
-        </div>
-    },
-    { 
-        appid: 6,
         icon: <Folder className="!h-[25px] !w-[25px] text-accent-icon"/>, 
         title: "My Projects" ,
         content: 
