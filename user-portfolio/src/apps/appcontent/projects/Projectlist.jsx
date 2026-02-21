4d666ee01c634b7bee771d32a178c72a57821583
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-21 20:33:49 +0800

    make new list for projects

---
+++ user-portfolio/src/apps/appcontent/projects/Projectlist.jsx
@@ -0,0 +1,52 @@
+import { Folder, User, ScanFace, Soup, Gamepad2, BusFront, BotMessageSquare } from "lucide-preact";
+import Optiface from "../../../assets/data/optiface.webp"
+import BakeBrewCook from "../../../assets/data/bbc.webp"
+import MathSolve from "../../../assets/data/mathsolve.webp"
+import Tipai from "../../../assets/data/tipai.webp"
+export const apps = [
+    { 
+        appid: 0,
+        icon: <ScanFace className="!h-[25px] !w-[25px] text-accent-icon"/>, 
+        title: "Opti-Face" ,
+        content: 
+        <div className="flex flex-col items-center">
+            A lightweight facial recognition application built by using scrfd, mediapipe's FaceMesh, and Edgeface that is still accurate and fast.
+        </div>
+    },
+    { 
+        appid: 1,
+        icon: <Soup className="!h-[25px] !w-[25px] text-accent-icon"/>, 
+        title: "Bake, Brew and Cook" ,
+        content: 
+        <div className="">
+            A small recipe application that contains a variety of Filipino recipes for baking, brewing, and cooking.
+        </div>
+    },
+    { 
+        appid: 2,
+        icon: <Gamepad2 className="!h-[25px] !w-[25px] text-accent-icon"/>, 
+        title: "Math Solve" ,
+        content: 
+        <div className="">
+            A game that generates a random -+/x problem to solve within a time limit.
+        </div>
+    },
+    { 
+        appid: 3,
+        icon: <BusFront className="!h-[25px] !w-[25px] text-accent-icon"/>, 
+        title: "Magic C Transport" ,
+        content: 
+        <div className="">
+            A transportation booking application for Magic C Transport Services.
+        </div>
+    },
+    { 
+        appid: 4,
+        icon: <BotMessageSquare className="!h-[25px] !w-[25px] text-accent-icon"/>, 
+        title: "T.I.P.A.I" ,
+        content: 
+        <div className="">
+            A simple AI chatbot that responds to user's queries about the technological institute of the philippines.
+        </div>
+    },
+];
