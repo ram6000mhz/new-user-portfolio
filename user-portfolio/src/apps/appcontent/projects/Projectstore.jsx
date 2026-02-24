ad6b73a227efa8b34898e6e4d956c9262d13b19a
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-24 18:57:34 +0800

    add project store

--- user-portfolio/src/apps/appcontent/projects/Projectstore.jsx
+++ user-portfolio/src/apps/appcontent/projects/Projectstore.jsx
@@ -1,16 +1,23 @@
 import { Projectlist } from "./Projectlist";
 import { create } from "zustand";
 
-export const useProjectStore = create((set) => ({
+export const useProjectStore = create((set, get) => ({
   selectedId: null,
   isOpen: false,
   project: null,
   
-  setSelectedProject: (id) => set({ 
-    selectedId: id, 
-    isOpen: true,
-    project: Projectlist.find(p => p.appid === id) 
-  }),
+  setSelectedProject: (id) => {
+    // if(get().isOpen){
+    //   get().closeSidebar()
+    //   set({ isOpen: false})
+    //   return
+    // }
+    set({ 
+      selectedId: id, 
+      isOpen: true,
+      project: Projectlist.find(p => p.appid === id) 
+    })
+  },
   
   closeSidebar: () => set({ isOpen: false, selectedId: null, project: null }),
 }));
