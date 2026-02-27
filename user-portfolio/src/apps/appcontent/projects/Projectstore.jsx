a02eb9754f00b3ac53b5605f202ce9726e125031
ram6000mhz <ethansanchoyap@gmail.com>
2026-02-27 19:53:33 +0800

    add pop up transition

--- user-portfolio/src/apps/appcontent/projects/Projectstore.jsx
+++ user-portfolio/src/apps/appcontent/projects/Projectstore.jsx
@@ -7,16 +7,24 @@ export const useProjectStore = create((set, get) => ({
   project: null,
   
   setSelectedProject: (id) => {
-    // if(get().isOpen){
-    //   get().closeSidebar()
-    //   set({ isOpen: false})
-    //   return
-    // }
+    const { isOpen, selectedId } = get();
+    if (isOpen && selectedId === id) return;
+    if (isOpen) {
+      set({ isOpen: false });
+      setTimeout(() => {
+        set({ 
+          selectedId: id, 
+          isOpen: true, 
+          project: Projectlist.find(p => p.appid === id) 
+        });
+      }, 300);
+      return; 
+    }
     set({ 
       selectedId: id, 
-      isOpen: true,
+      isOpen: true, 
       project: Projectlist.find(p => p.appid === id) 
-    })
+    });
   },
   
   closeSidebar: () => set({ isOpen: false }),
