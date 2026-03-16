8bb2bb3ac815542c55cf826045523dbd9eb6613f
ram6000mhz <ethansanchoyap@gmail.com>
2026-03-16 19:02:13 +0800

    make intro responsive

--- user-portfolio/src/apps/appcontent/aboutme/Intro.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Intro.jsx
@@ -3,20 +3,20 @@ import { Contacables } from "./Contacables";
 export const Intro = () =>{
     return(
         <div className="flex flex-row w-full h-full items-center justify-center">
-            <div className="bg-black flex-1 h-full grid place-content-center justify-items-start">
-                <h1 className="font-bold text-5xl text-white">
+            <div className="relative z-10 bg-black/30 backdrop-blur-xs flex-1 h-full grid place-content-center justify-items-start">
+                <h1 className="font-bold text-[clamp(1.5rem,8vw,3rem)] text-white">
                    Ethan Sancho Yap
                 </h1>
-                <h2 className="font-semibold text-xl text-muted-text">
+                <h2 className="font-semibold text-[clamp(1rem,4vw,1.25rem)] text-muted-text">
                     Software developer
                 </h2>
-                <h3 className="font-extralight text-md text-muted-text">
+                <h3 className="font-extralight text-[clamp(0.875rem,3vw,1rem)] text-muted-text">
                     I enjoy building interesting projects.
                 </h3>
                 <Contacables/>
             </div>
-            <div className="bg-black flex-1 h-full ">
-                <Hero3d></Hero3d>
+            <div className="absolute inset-0 md:relative md:flex-1 h-full w-full bg-black z-0">
+                <Hero3d />
             </div>
         </div>
     )
