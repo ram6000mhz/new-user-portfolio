1e8a313225414a0a46ac072003669954066fc416
ram6000mhz <ethansanchoyap@gmail.com>
2026-03-30 21:34:33 +0800

    add hero3d worker

---
+++ user-portfolio/src/apps/appcontent/aboutme/Heroworker3d.js
@@ -0,0 +1,121 @@
+import { 
+  WebGLRenderer, Scene, PerspectiveCamera, TorusGeometry, 
+  LineSegments, LineBasicMaterial, EdgesGeometry , EventDispatcher
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+class ElementProxyReceiver extends EventDispatcher {
+    constructor() {
+      super();
+      this.style = {};
+      this.width = 0;
+      this.height = 0;
+      this.left = 0;
+      this.top = 0;
+    }
+    setPointerCapture() { }
+    releasePointerCapture() { }
+    getRootNode() {
+      return this; 
+    }
+
+    get clientWidth() {
+      return this.width;
+    }
+
+    get clientHeight() {
+      return this.height;
+    }
+
+    getBoundingClientRect() {
+      return {
+        left: this.left,
+        top: this.top,
+        width: this.width,
+        height: this.height,
+      };
+    }
+
+    focus() {}
+    
+    ownerDocument = this;
+}
+const proxy = new ElementProxyReceiver();
+
+let renderer, scene, camera, wireframe, frameId, controls;
+let isInteracting = false;
+
+self.onmessage = (e) => {
+    const { type, data, canvas, width, height, pixelRatio } = e.data;
+
+    if (type === 'init') {
+      proxy.width = width;
+      proxy.height = height;
+
+      renderer = new WebGLRenderer({ canvas, antialias: false, alpha: true });
+      renderer.setPixelRatio(pixelRatio);
+      renderer.setSize(width, height, false);
+
+      scene = new Scene();
+      camera = new PerspectiveCamera(75, width / height, 0.1, 100);
+      camera.position.set(0, 3, 5);
+      controls = new OrbitControls(camera, proxy);
+      controls.enableDamping = true; 
+
+      controls.addEventListener('start', () => {
+        isInteracting = true;
+      });
+
+      controls.addEventListener('end', () => {
+        isInteracting = false;
+      });
+
+      const geo = new TorusGeometry(2, 1, 15, 15);
+      const edges = new EdgesGeometry(geo);
+      wireframe = new LineSegments(edges, new LineBasicMaterial({ color: 0xffffff, transparent: true, opacity: 0.6 }));
+      scene.add(wireframe);
+
+      render();
+    }
+
+    if (type === 'event') {
+      proxy.dispatchEvent(data);
+    }
+
+    if (type === 'resize') {
+      if (renderer.domElement.width !== width || renderer.domElement.height !== height) {  
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(width, height, false);
+        renderer.render(scene, camera);
+      }
+    }
+
+    if (type === 'stop') {
+      cancelAnimationFrame(frameId);
+      scene.traverse((object) => {
+      if (object.geometry) object.geometry.dispose();
+        if (object.material) {
+          if (Array.isArray(object.material)) {
+            object.material.forEach(mat => mat.dispose());
+          } else {
+            object.material.dispose();
+          }
+        }
+      });
+      renderer.dispose();
+    }
+};
+
+function render() {
+    frameId = requestAnimationFrame(render);
+    if (controls) {
+        controls.update();
+    }
+    if (!isInteracting) {
+        wireframe.rotation.x += 0.005;
+        wireframe.rotation.z += 0.003;
+    }
+    renderer.render(scene, camera);
+}
