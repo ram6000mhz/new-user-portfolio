24b4989bef12df51610d260c7aafbef1fe80bf0f
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-02 20:53:19 +0800

    use flag icon package

--- user-portfolio/src/main.jsx
+++ user-portfolio/src/main.jsx
@@ -1,6 +1,7 @@
 import { hydrate, prerender as ssr } from 'preact-iso';
 import './index.css'
 import { App } from './App.jsx'
+import "flag-icons/css/flag-icons.min.css";
 
 if (typeof window !== 'undefined') {
     hydrate(<App />, document.getElementById('app'));
