70d7f0f190c048e0169545981f0bffc74c58b80c
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-03 19:19:26 +0800

    remove unoptimal import of flags

--- user-portfolio/src/main.jsx
+++ user-portfolio/src/main.jsx
@@ -1,7 +1,6 @@
 import { hydrate, prerender as ssr } from 'preact-iso';
 import './index.css'
 import { App } from './App.jsx'
-import "flag-icons/css/flag-icons.min.css";
 
 if (typeof window !== 'undefined') {
     hydrate(<App />, document.getElementById('app'));
