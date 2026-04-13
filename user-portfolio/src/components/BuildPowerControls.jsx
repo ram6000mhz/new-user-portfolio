52dd1228364651e1e907e7acc54770f5a8e230a5
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-13 21:19:03 +0800

    replace div with fragment

--- user-portfolio/src/components/BuildPowerControls.jsx
+++ user-portfolio/src/components/BuildPowerControls.jsx
@@ -7,28 +7,27 @@ export const BuildPowerControls = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleHrMode = ViewHandler((state) => state.toggleHrMode);
     const powerRef = useRef(null);
-    console.log("hi")
     return(
-        <div>
+        <>
             <div ref={powerRef} onClick={() => setIsMenuOpen(!isMenuOpen)} className="w-[25px] h-[25px] p-1 hover:bg-foreground-highlight flex items-center justify-center rounded-2xl cursor-pointer relative">
                 <Power className="!w-[20px] !h-[20px] text-accent-icon"/>
             </div>
             {isMenuOpen && createPortal(
                 <div
                     className={`h-auto flex flex-col
-                    absolute bottom-full rounded-lg bg-foreground
-                    overflow-hidden border border-muted-border `}
+                    absolute bottom-full rounded-md bg-foreground
+                    overflow-hidden border border-muted-border p-1 gap-1`}
                 >
-                    <div onClick={toggleHrMode} className="flex flex-row w-full h-full items-center">
-                        <Power className="!w-[15px] !h-[15px] text-accent-icon"/>
-                        <p className="text-xs text-white">Shutdown</p>
+                    <div onClick={toggleHrMode} className="flex flex-row w-full h-full items-center gap-1">
+                        <Power className="!w-[12px] !h-[12px] text-accent-icon"/>
+                        <p className="text-[8px] text-white">Shutdown</p>
                     </div>
-                    <div onClick={toggleHrMode} className="flex flex-row w-full h-full items-center">
-                        <RotateCcw className="!w-[15px] !h-[15px] text-accent-icon"/>
-                        <p className="text-xs text-white">Restart</p>
+                    <div onClick={toggleHrMode} className="flex flex-row w-full h-full items-center gap-1">
+                        <RotateCcw className="!w-[12px] !h-[12px] text-accent-icon"/>
+                        <p className="text-[8px] text-white">Restart</p>
                     </div>
                 </div>
             ,powerRef.current)}
-        </div>
+        </>
     )
 }
