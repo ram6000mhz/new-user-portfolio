9a0b4fd97f170076e7b0aac7fd5e761424aa0045
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-15 21:31:19 +0800

    fix icon onhover highlight bubbling onto child, add cursor pointers for context menu options and highlights, change component setup

--- user-portfolio/src/components/BuildPowerControls.jsx
+++ user-portfolio/src/components/BuildPowerControls.jsx
@@ -9,20 +9,26 @@ export const BuildPowerControls = () => {
     const powerRef = useRef(null);
     return(
         <>
-            <div ref={powerRef} onClick={() => setIsMenuOpen(!isMenuOpen)} className="w-[25px] h-[25px] p-1 hover:bg-foreground-highlight flex items-center justify-center rounded-2xl cursor-pointer relative">
-                <Power className="!w-[20px] !h-[20px] text-accent-icon"/>
+            <div ref={powerRef} className="w-[35px] h-[35px] relative flex items-center justify-center">
+                <div onClick={() => setIsMenuOpen(!isMenuOpen)} 
+                className="w-full h-full hover:bg-foreground-highlight flex 
+                items-center justify-center rounded-sm cursor-pointer p-0.5
+                ">
+                    <Power size={20} className="text-accent-icon"/>
+                </div>
             </div>
             {isMenuOpen && createPortal(
                 <div
                     className={`h-auto flex flex-col
                     absolute bottom-full rounded-md bg-foreground
-                    overflow-hidden border border-muted-border p-2 gap-1`}
+                    overflow-hidden border border-muted-border p-2 gap-1
+                    mb-[10px]`}
                 >
-                    <div onClick={toggleHrMode} className="flex flex-row w-full h-full items-center gap-1 px-1 hover:bg-foreground-highlight rounded-sm">
+                    <div onClick={toggleHrMode} className="flex flex-row items-center gap-1 px-1 hover:bg-foreground-highlight rounded-sm cursor-pointer">
                         <Power className="!w-[15px] !h-[15px] text-accent-icon"/>
                         <p className="text-[12px] text-white">Shutdown</p>
                     </div>
-                    <div onClick={toggleHrMode} className="flex flex-row w-full h-full items-center gap-1 px-1 hover:bg-foreground-highlight rounded-sm">
+                    <div onClick={toggleHrMode} className="flex flex-row items-center gap-1 px-1 hover:bg-foreground-highlight rounded-sm cursor-pointer">
                         <RotateCcw className="!w-[15px] !h-[15px] text-accent-icon"/>
                         <p className="text-[12px] text-white">Restart</p>
                     </div>
