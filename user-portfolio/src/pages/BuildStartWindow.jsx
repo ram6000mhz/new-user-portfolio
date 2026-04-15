dab996e36d42c23354a5516ebff512895acba4b3
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-15 21:29:43 +0800

    fix icon onhover highlight bubbling onto child and component setup

--- user-portfolio/src/pages/BuildStartWindow.jsx
+++ user-portfolio/src/pages/BuildStartWindow.jsx
@@ -7,25 +7,28 @@ export const BuildStartWindow = ()=>{
     const [isVisible, setIsVisible] = useState(false);
     const monitorRef = useRef(null);
     return(
-        <div className="relative">
+        <>
             <div ref={monitorRef} className="flex justify-center items-center 
-                h-[35px] w-[35px] bg-foreground hover:bg-foreground-highlight rounded cursor-pointer"
-                onClick={(e) => {
-                    setIsVisible(!isVisible);
-                }}
+                h-[35px] w-[35px] bg-foreground relative"
             >
-                <Monitor className="text-accent-icon"/>
+                <div className="flex justify-center items-center rounded w-full h-full hover:bg-foreground-highlight cursor-pointer"
+                    onClick={(e) => {
+                        setIsVisible(!isVisible);
+                    }}
+                >
+                    <Monitor className="text-accent-icon"/>
+                </div>
             </div>
             {isVisible && createPortal(
                 <div
                     onClick={(e) => e.stopPropagation()}
-                    className={`h-auto w-[clamp(180px,25vw,500px)] transition-all
-                    duration-300 ease-in-out absolute bottom-[55px] left-0 rounded-lg bg-background 
-                    overflow-hidden border border-muted-border`}
+                    className={`h-auto w-[clamp(180px,25vw,400px)]
+                    bottom-[55px] left-0 rounded-lg bg-background 
+                    overflow-hidden border border-muted-border absolute`}
                 >
                     <StartComponent/>
                 </div>
             ,monitorRef.current)}
-        </div>
+        </>
     )
 }
