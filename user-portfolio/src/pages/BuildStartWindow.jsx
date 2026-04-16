b77671febb003dd1d8a8ae68f8a730586090c6ca
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-16 23:09:55 +0800

    make downward animation working

--- user-portfolio/src/pages/BuildStartWindow.jsx
+++ user-portfolio/src/pages/BuildStartWindow.jsx
@@ -5,22 +5,36 @@ import { createPortal } from "preact/compat";
 import { useRef } from "preact/hooks";
 export const BuildStartWindow = ()=>{
     const [isVisible, setIsVisible] = useState(false);
+    const [isRendered, setIsRendered] = useState(false);
     const monitorRef = useRef(null);
+
+    const handleToggle = () => {
+        if (!isVisible) {
+            setIsRendered(true);
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    }
+
+    const handleAnimationEnd = () => {
+        if (!isVisible) setIsRendered(false);
+    };
+
     return(
         <>
             <div ref={monitorRef} className="flex justify-center items-center 
                 h-[35px] w-[35px] bg-foreground relative"
             >
                 <div className="flex justify-center items-center rounded w-full h-full hover:bg-foreground-highlight cursor-pointer"
-                    onClick={(e) => {
-                        setIsVisible(!isVisible);
-                    }}
+                    onClick={handleToggle}
                 >
                     <Monitor className="text-accent-icon"/>
                 </div>
             </div>
-            {isVisible && createPortal(
+            {isRendered && createPortal(
                 <div
+                    onAnimationEnd={handleAnimationEnd}
                     onClick={(e) => e.stopPropagation()}
                     className={`h-auto w-[clamp(180px,25vw,400px)]
                     bottom-[55px] left-0 rounded-lg bg-background 
