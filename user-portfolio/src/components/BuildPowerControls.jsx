2edd1e4ad2829adee7cbc09892e524b763d5d9b2
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-17 19:14:14 +0800

    add animation for power controls

--- user-portfolio/src/components/BuildPowerControls.jsx
+++ user-portfolio/src/components/BuildPowerControls.jsx
@@ -6,23 +6,40 @@ import { useRef } from "preact/hooks";
 export const BuildPowerControls = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const toggleHrMode = ViewHandler((state) => state.toggleHrMode);
+    const [isRendered, setIsRendered] = useState(false);
     const powerRef = useRef(null);
+
+    const handleToggle = () => {
+        if (!isMenuOpen) {
+            setIsRendered(true);
+            setIsMenuOpen(true);
+        } else {
+            setIsMenuOpen(false);
+        }
+    }
+
+    const handleAnimationEnd = () => {
+        if (!isMenuOpen) setIsRendered(false);
+    };
+
+
     return(
         <>
             <div ref={powerRef} className="w-[35px] h-[35px] relative flex items-center justify-center">
-                <div onClick={() => setIsMenuOpen(!isMenuOpen)} 
+                <div onClick={handleToggle} 
                 className="w-full h-full hover:bg-foreground-highlight flex 
                 items-center justify-center rounded-sm cursor-pointer p-0.5
                 ">
                     <Power size={20} className="text-accent-icon"/>
                 </div>
             </div>
-            {isMenuOpen && createPortal(
+            {isRendered && createPortal(
                 <div
+                    onAnimationEnd={handleAnimationEnd}
                     className={`h-auto flex flex-col
                     absolute bottom-full rounded-md bg-foreground
                     overflow-hidden border border-muted-border p-2 gap-1
-                    mb-[10px]`}
+                    mb-[10px] ${isMenuOpen ? "animate-window-in" : "animate-window-out"}`}
                 >
                     <div onClick={toggleHrMode} className="flex flex-row items-center gap-1 px-1 hover:bg-foreground-highlight rounded-sm cursor-pointer">
                         <Power className="!w-[15px] !h-[15px] text-accent-icon"/>
