03ad3d4419416436b9c03c103147a2e8fe74193f
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-24 20:56:01 +0800

    make cannon clas

--- user-portfolio/src/apps/WindowComponent.jsx
+++ user-portfolio/src/apps/WindowComponent.jsx
@@ -19,19 +19,19 @@ export const WindowComponent=({title, isFullscreen, terminationcallback, windowc
     return(
         <div className="w-full h-full bg-background flex flex-col">
             <div className="window-header flex flex-row items-center justify-center bg-foreground 
-                border-b-2 border-muted-border w-full h-[40px] p-2 gap-1 @container cursor-move">
+                border-b-2 border-muted-border w-full h-10 p-2 gap-1 @container cursor-move">
                 <h1 className="text-[10px] @sm:text-xs @md:text-sm @lg:text-base @xl:text-lg text-accent-text">   
                     {title}
                 </h1>
                 <div className="grow"/>
                 <div className="cursor-pointer flex flex-col items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]" onClick={Minimize} onTouchEnd={(e)=>{e.preventDefault();Minimize();}}>
-                    <Minus className="!h-[15px] !w-[15px] text-accent-icon"/>
+                    <Minus className="h-[15px]! w-[15px]! text-accent-icon"/>
                 </div>
                 <div className="cursor-pointer flex items-center justify-center hover:bg-foreground-highlight rounded h-full w-[30px]" onClick={Window} onTouchEnd={(e)=>{e.preventDefault();Window();}}>
-                    {isFullscreen ? <PictureInPicture2 className="!h-[15px] !w-[15px] rotate-180 scale-y-[-1] text-accent-icon"/>: <Square className="!h-[15px] !w-[15px] text-accent-icon"/>}
+                    {isFullscreen ? <PictureInPicture2 className="h-[15px]! w-[15px]! rotate-180 scale-y-[-1] text-accent-icon"/>: <Square className="!h-[15px] !w-[15px] text-accent-icon"/>}
                 </div>
                 <div className="cursor-pointer flex items-center justify-center hover:bg-red-300 rounded h-full w-[30px]" onClick={Terminate} onTouchEnd={(e)=>{e.preventDefault();Terminate();}}>
-                    <X className="!h-[15px] !w-[15px] text-accent-icon"/>
+                    <X className="h-[15px]! w-[15px]! text-accent-icon"/>
                 </div>
             </div>
             <div className="deadzone w-full flex-1 min-h-0" onClick={()=>{bringToFront(appId)}} onTouchEnd={(e)=>{e.preventDefault();bringToFront(appId);}}>
