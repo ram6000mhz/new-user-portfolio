e095f491e33570cd1e89d6c105a185424a95de9b
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-27 23:18:58 +0800

    adjust y padding

--- user-portfolio/src/pages/DesktopFooter.jsx
+++ user-portfolio/src/pages/DesktopFooter.jsx
@@ -4,7 +4,7 @@ import { Timer } from "./Timer";
 export const DesktopFooter = () => {
 
     return (
-        <footer className="w-full h-[50px] flex flex-row items-center border-t-2 border-muted-border px-2 sm:px-2 md:px-4 lg:px-6 bg-foreground z-50">
+        <footer className="w-full h-[50px] flex flex-row items-center border-t-2 border-muted-border py-1 px-2 sm:px-2 md:px-4 lg:px-6 bg-foreground z-50">
             <BuildStartWindow/>
             <TaskBar/>
             <div className="grow"></div>
