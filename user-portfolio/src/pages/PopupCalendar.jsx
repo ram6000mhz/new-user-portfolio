394ed2fc93d44c2ad188892344dadb72b350f960
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-28 19:24:21 +0800

    make calendar device

---
+++ user-portfolio/src/pages/PopupCalendar.jsx
@@ -0,0 +1,7 @@
+export const PopupCalendar = () => {
+    return(
+        <div className="w-[250px] h-[250px] bg-foreground border-muted-border border-2 rounded-xl absolute bottom-0 right-0 z-50 p-2 m-2">
+            claendar
+        </div>
+    )
+}
