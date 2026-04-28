ebf1a50765020b7e4352c08d16bae8f3e42b1c1f
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-28 19:25:08 +0800

    add calendarhandler toggle

--- user-portfolio/src/pages/Timer.jsx
+++ user-portfolio/src/pages/Timer.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "preact/hooks";
-
+import { CalendarHandler } from "../providers/CalendarHandler";
 export const Timer = ()=>{
     const [time,setTime] = useState(new Date())
-
+    const toggleCalendar = CalendarHandler((state) => state.toggleCalendar);
     useEffect(() => {
         const timer = setInterval(() => {
         setTime(new Date());
@@ -14,7 +14,7 @@ export const Timer = ()=>{
     const date = time.toLocaleDateString();
 
     return(
-        <div className="flex flex-col items-center justify-center h-full px-1 hover:bg-foreground-highlight cursor-pointer rounded-sm">
+        <div className="flex flex-col items-center justify-center h-full px-1 hover:bg-foreground-highlight cursor-pointer rounded-sm select-none" onClick={toggleCalendar}>
             <time className="text-xs sm:text-sm text-accent-text">{now}</time>
             <time className="text-xs sm:text-sm text-accent-text">{date}</time>
         </div>
