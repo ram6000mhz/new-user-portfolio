6df5d8845a38ca85fe3932ba6ab81fa165aac45a
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-29 18:35:54 +0800

    add shutdown calendar

--- user-portfolio/src/providers/CalendarHandler.jsx
+++ user-portfolio/src/providers/CalendarHandler.jsx
@@ -4,5 +4,10 @@ export const CalendarHandler = create((set,get)=>({
     enableCalendar: false,
     toggleCalendar(){
         set((state) => ({ enableCalendar: !state.enableCalendar}));
+    },
+    shutdownCalendar(){
+        if(get().enableCalendar){
+            set({enableCalendar:false});
+        }
     }
 }))
