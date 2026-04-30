6a68c77bf37728552be341bcbc4f4ce6f9cee961
ram6000mhz <ethansanchoyap@gmail.com>
2026-04-30 18:02:23 +0800

    add content on calener

--- user-portfolio/src/pages/PopupCalendar.jsx
+++ user-portfolio/src/pages/PopupCalendar.jsx
@@ -1,7 +1,23 @@
+import { useMemo } from 'preact/hooks';
+
 export const PopupCalendar = () => {
+
+    const formattedDate = useMemo(() => {
+    return new Intl.DateTimeFormat('en-US', {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    }).format(new Date());
+    }, []);
+
     return(
-        <div className="w-[250px] h-[250px] bg-foreground border-muted-border border-2 rounded-xl absolute bottom-0 right-0 z-50 p-2 m-2">
-            claendar
+        <div className="flex flex-col w-[250px] h-[300px] bg-foreground border-muted-border border-2 rounded-xl absolute bottom-0 right-0 z-50 m-2 overflow-hidden">
+            <div className="flex w-full h-[50px] bg-foreground items-center justify-start text-white pl-2 text-sm">
+                {formattedDate}
+            </div>
+            <div className="grow bg-background">
+
+            </div>
         </div>
     )
 }
