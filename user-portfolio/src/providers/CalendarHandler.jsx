9da26d6ee4639ed0a08a6cbdac034d30f77d4577
ram6000mhz <ethansanchoyap@gmail.com>
2026-05-01 20:55:55 +0800

    add new functions and states to calendar handler to handle calendar animations

--- user-portfolio/src/providers/CalendarHandler.jsx
+++ user-portfolio/src/providers/CalendarHandler.jsx
@@ -2,12 +2,25 @@ import { create } from 'zustand';
 
 export const CalendarHandler = create((set,get)=>({
     isCalendarVisible: false,
+    isCalendarRendered: false,
+
     toggleCalendar(){
-        set((state) => ({ isCalendarVisible: !state.isCalendarVisible}));
+        if(!get().isCalendarVisible){
+            set((state) => ({ isCalendarRendered: true}));
+            set((state) => ({ isCalendarVisible: true}));
+        }else{
+            set((state) => ({ isCalendarVisible: false}));
+        }
     },
+
+    setIsCalendarRendered(){
+        if(!get().isCalendarVisible) set((state) => ({ isCalendarRendered: false}));
+    },
+
     shutdownCalendar(){
         if(get().isCalendarVisible){
             set({isCalendarVisible:false});
+            set({isCalendarRendered:false})
         }
     }
 }))
