36e92b48fc5d222c3f596b4390b2ac52575847a1
ram6000mhz <ethansanchoyap@gmail.com>
2026-07-02 20:32:55 +0800

    brainstorming new component that shows experience

---
+++ user-portfolio/src/apps/appcontent/aboutme/Experience.jsx
@@ -0,0 +1,8 @@
+export const Experience = () => {
+    return(
+        <div className="w-full items-center flex justify-center h-[60px] bg-white">
+            Experience
+            Current experience coding expert at Data annotations
+        </div>
+    )
+}
