9b6b23d06d0681e34ee3aebddab884c86981bf1b
ram6000mhz <ethansanchoyap@gmail.com>
2026-07-03 19:46:20 +0800

    add groundwork on what this will look like

--- user-portfolio/src/apps/appcontent/aboutme/Experience.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Experience.jsx
@@ -1,8 +1,37 @@
 export const Experience = () => {
     return(
-        <div className="w-full items-center flex justify-center h-[60px] bg-white">
-            Experience
-            Current experience coding expert at Data annotations
+        <div className="w-full items-center flex flex-col justify-center gap-2 px-60 bg-white">
+            <div className="w-full flex  items-start">
+                My Experiences
+            </div>
+            <div className="flex flex-col items-start w-full border">
+                <h1>
+                    Coding Expert Data Annotator
+                </h1>
+                <h2>
+                    DataAnnotationsTech
+                </h2>
+                <h3>
+                    2026-Current
+                </h3>
+                <p>
+                    Contributing in improving frontier AI models.
+                </p>
+            </div>
+            <div className="flex flex-col items-start w-full border">
+                <h1>
+                    Intern Web programmer
+                </h1>
+                <h2>
+                    Moodlearning
+                </h2>
+                <h3>
+                    2025
+                </h3>
+                <p>
+                    Successfuly implemented new features for a nationwide spanning application.
+                </p>
+            </div>
         </div>
     )
 }
