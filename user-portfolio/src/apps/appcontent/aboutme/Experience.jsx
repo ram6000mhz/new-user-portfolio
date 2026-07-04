fbcd486e3c44f8c7e035e96dc6704d48bf41e45e
ram6000mhz <ethansanchoyap@gmail.com>
2026-07-04 20:10:56 +0800

    initial design

--- user-portfolio/src/apps/appcontent/aboutme/Experience.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Experience.jsx
@@ -1,20 +1,22 @@
 export const Experience = () => {
     return(
-        <div className="w-full items-center flex flex-col justify-center gap-2 px-60 bg-white">
+        <div className="w-full items-center flex flex-col gap-2 px-4 max-w-4xl bg-black mx-auto">
             <div className="w-full flex  items-start">
-                My Experiences
+                <h1 className="font-semibold text-xl text-white text-start">
+                    My Experiences
+                </h1>
             </div>
-            <div className="flex flex-col items-start w-full border">
-                <h1>
+            <div className="flex flex-col items-start w-full border border-zinc-400 rounded-2xl p-2">
+                <h1 className="font-semibold text-xl text-white text-start">
                     Coding Expert Data Annotator
                 </h1>
-                <h2>
+                <h2 className="text-md text-white text-start">
                     DataAnnotationsTech
                 </h2>
-                <h3>
-                    2026-Current
+                <h3 className=" text-sm text-zinc-400 text-start">
+                    2026 - Current
                 </h3>
-                <p>
+                <p className="text-sm text-white text-start pt-2">
                     Contributing in improving frontier AI models.
                 </p>
             </div>
