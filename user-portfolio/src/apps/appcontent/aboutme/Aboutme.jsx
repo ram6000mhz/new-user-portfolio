0c31a8398dd6544494fd7a6bb688cbc26c028a14
ram6000mhz <ethansanchoyap@gmail.com>
2026-07-08 20:15:44 +0800

    add forward refs to pass component information to higher lvl component

--- user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
+++ user-portfolio/src/apps/appcontent/aboutme/Aboutme.jsx
@@ -1,12 +1,12 @@
 import { lazy, ErrorBoundary } from 'preact-iso';
 import { Intro } from "./Intro";
 import { Experience } from './Experience';
-
+import { forwardRef } from 'preact/compat';
 const Skillchart = lazy(() => import('./Skillchart').then(m => m.Skillchart));
 
-export const Aboutme = ()=>{
+export const Aboutme = forwardRef(({ onScroll }, ref)=>{
     return(
-        <div className="flex w-full h-full flex-row overflow-auto overscroll-none no-scrollbar select-none bg-black">
+        <div onScroll={onScroll} ref={ref} className="flex w-full h-full flex-row overflow-auto overscroll-none no-scrollbar select-none bg-black">
             <div className="flex-1 flex-col items-center h-full relative">
                 <Intro/>
                 <ErrorBoundary>
@@ -16,4 +16,4 @@ export const Aboutme = ()=>{
             </div>    
         </div> 
     )
-}
+})
