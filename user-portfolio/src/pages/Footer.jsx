160d06a3faf14cd51c362282ba1fcd086036a351
ram6000mhz <ethansanchoyap@gmail.com>
2026-07-11 20:43:13 +0800

    add footer contents and auto year update

--- user-portfolio/src/pages/Footer.jsx
+++ user-portfolio/src/pages/Footer.jsx
@@ -1,9 +1,17 @@
+import { useState, useEffect } from "preact/hooks";
 export const Footer = () => {
+    const [year, setYear] = useState('');
+
+    useEffect(() => {
+        setYear(new Date().getFullYear().toString());
+    }, []);
     return (
         <div 
-            className='flex w-full h-[100px] bg-black text-white justify-center items-center'
+            className='flex flex-col w-full h-[100px] bg-black text-white justify-center items-center py-4 gap-4 border-t border-zinc-900'
         >
-            footer
+            <div className="flex flex-row text-white">
+                © {year} Ethan-dev.me. All rights reserved.
+            </div>
         </div>
     );
 };
